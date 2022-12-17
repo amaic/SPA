64b515ebceec61cf9c4830a367270221e0ef26bb
Alexander Mauri <maurialex@web.de>
2022-12-17 19:45:33 +0100

    init

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.debug("test");
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import { Configuration } from "webpack";
+import path from "path";
+
+
+const config: Configuration =
+{
+    mode: "development",
+    devtool: "source-map",
+
+    entry: "./src/index.ts",
+    output: {
+        clean: true,
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: 'babel-loader',
+            }
+        ],
+    },
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
+
+export default config;
